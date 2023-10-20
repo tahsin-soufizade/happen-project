5d908d45dd5b37f59a3eb415547028429934cd58
tahsin soufizade <soufilearn@gmail.com>
2023-10-20 13:43:07 +0330

    Done Main Page

--- js/main.js
+++ js/main.js
@@ -34,7 +34,8 @@ window.addEventListener("scroll", (e) => {
   }
 });
 
-const swiper = new Swiper(".comments-slider", {
+// COMMENTS SWIPPER
+const commentSlider = new Swiper(".comments-slider", {
   slidesPerView: 1,
   spaceBetween: 60,
   breakpoints: {
@@ -52,4 +53,75 @@ const swiper = new Swiper(".comments-slider", {
     el: ".swiper-scrollbar",
   },
 });
-swiper.update();
+commentSlider.update();
+
+// TOGGLE BETWEEN QUESTIONS ANSWER
+const questionItems = document.querySelectorAll(".question-item");
+const answerBoxs = document.querySelectorAll(".answer-box");
+
+questionItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    const answerBox = item.querySelector(".answer-box");
+    const answerText = item.querySelector(".answer-text");
+
+    // GET EACH HEIGHT ANSWER
+    const answerHeight = answerText.getBoundingClientRect().height;
+
+    if (item.classList.contains("showAnswer")) {
+      resetAnswer();
+    } else {
+      resetAnswer();
+      item.classList.add("showAnswer");
+      answerBox.style.height = `${answerHeight}px`;
+    }
+  });
+});
+
+// RESET ANSWERS WHEN CLICK TO ANOTHER QUESTION
+const resetAnswer = () => {
+  answerBoxs.forEach((answer) => {
+    const questionItem = answer.parentElement;
+    questionItem.classList.remove("showAnswer");
+    answer.style.height = 0;
+  });
+};
+
+// COMMENTS SWIPPER
+const weblogSlider = new Swiper(".weblogs-slider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  speed: 1200,
+
+  autoplay: true,
+});
+
+weblogSlider.update();
+
+// GO TOP BUTTON
+const goToTopButton = document.querySelector(".go-top-btn");
+
+// show button when scroll arrived at the middle of the site
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 1200) {
+    goToTopButton.classList.add("show-go-top-btn");
+  } else {
+    goToTopButton.classList.remove("show-go-top-btn");
+  }
+});
+
+// when button clicked, back to top
+goToTopButton.addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
